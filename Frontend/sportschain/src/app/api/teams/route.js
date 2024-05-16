e4eb1ad628fbc9d665273265a1181549d628d36c
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-05-16 20:43:49 +0200

    added statistics when fetching with only one season

--- Frontend/sportschain/src/app/api/teams/route.js
+++ Frontend/sportschain/src/app/api/teams/route.js
@@ -2,7 +2,7 @@
 export async function GET() {
   try {
     const res = await fetch(
-      `https://api.sportmonks.com/v3/football/teams/seasons/21644?api_token=${process.env.SPORTMONK_API_TOKEN}&include=statistics.details.type`
+      `https://api.sportmonks.com/v3/football/teams/seasons/21644?api_token=${process.env.SPORTMONK_API_TOKEN}&include=statistics.details.type&filters=teamStatisticSeasons:21644`
     );
     const teamsData = await res.json();
     console.log(teamsData);
