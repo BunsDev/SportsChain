0a76fd954e5a9d764a25384b96828f141411c06e
Gérald Quenum <geraldquenum9@gmail.com>
2024-05-17 20:16:58 +0200

    redirection account + doc

--- Frontend/sportschain/pages/account.js
+++ Frontend/sportschain/pages/account.js
@@ -4,6 +4,7 @@ import { useAuth } from "../context/authContext";
 
 export default function Account() {
   const { user, disconnect } = useAuth();
+  console.log('User:', user);
 
   return (
     <Box bg="black" color="white" minH="100vh" p={4}>
@@ -14,6 +15,7 @@ export default function Account() {
         <>
           <Text fontSize="xl" mb={4}>Name: {user.name}</Text>
           <Text fontSize="xl" mb={4}>Email: {user.email}</Text>
+          <image fontSize="xl" mb={4}> image: {user.profileImage}</image>
           <Button colorScheme="red" size="lg" onClick={disconnect}>
             Logout
           </Button>
