635c9f4d76321cb228b244d3c0eeb302b7862037
mouhamadou <mouhamadoul.sarr@gmail.com>
2024-05-18 13:43:21 +0200

    additin of standings modal component in the teams page

--- Frontend/sportschain/src/app/teams/page.js
+++ Frontend/sportschain/src/app/teams/page.js
@@ -1,8 +1,13 @@
 "use client";
 import { useState, useEffect } from "react";
 import Team from "../components/Team";
+import StandingsModal from "../components/StandingsModal";
+
 const Teams = () => {
   const [teamsData, setTeamsData] = useState([]);
+
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     const fetchTeams = async () => {
       try {
@@ -17,10 +22,24 @@ const Teams = () => {
   }, []);
 
   return (
-    <div className="flex flex-wrap">
-      {teamsData.map(team => (
-        <Team key={team.id} team={team} />
-      ))}
+    <div className="relative">
+      <div className="flex flex-wrap gap-6 mx-auto w-11/12 my-6 items-center justify-center">
+        <h1 className="text-white text-center text-4xl  ">
+          Danish Super League Teams
+        </h1>
+        <button
+          onClick={() => setShowModal(true)}
+          className="bg-white  py-1 px-3 rounded-md"
+        >
+          Get Standings
+        </button>
+      </div>
+      <div className="flex flex-wrap gap-6 mx-auto w-11/12">
+        {teamsData.map(team => (
+          <Team key={team.id} team={team} />
+        ))}
+      </div>
+      {showModal && <StandingsModal setShowModal={setShowModal} />}
     </div>
   );
 };
