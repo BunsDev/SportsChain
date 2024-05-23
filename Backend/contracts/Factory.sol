a57d5b06e26605485eaa4ebed9af8c661a950621
stormerino78 <tachot.adrien7@gmail.com>
2024-05-23 12:53:22 +0200

    Management of different token in a contract + correction calulate price

--- Backend/contracts/Factory.sol
+++ Backend/contracts/Factory.sol
@@ -1,54 +1,113 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.20;
 
-import "./SportsToken.sol";
+import "./token.sol"; // Import the token contract interface (or similar ERC20 interface)
 
-contract TokenFactory {
+contract TokenManager {
+    struct TokenInfo {
+        token token; // Token contract
+        uint256 tokenPrice; // Price of one token in wei
+    }
+
+    mapping(uint256 => TokenInfo) public tokens;
     address public owner;
-    SportsToken[] public deployedTokens;
+    bool public tradingBlocked;
+
+    event Bought(address indexed buyer, uint256 teamID, uint256 amount);
+    event Sold(address indexed seller, uint256 teamID, uint256 amount);
+    event PriceUpdated(uint256 teamID, uint256 oldPrice, uint256 newPrice);
+    event TradingBlocked();
+    event TradingUnblocked();
 
     constructor() {
         owner = msg.sender;
     }
-    function createToken(string memory name, string memory symbol) public {
-        require(msg.sender == owner, "Only owner can create new tokens");
-        SportsToken newToken = new SportsToken(name, symbol);
-        deployedTokens.push(newToken);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
     }
 
-    function getDeployedTokens() public view returns (SportsToken[] memory) {
-        return deployedTokens;
+    modifier tradingNotBlocked() { //affect the lock to the sell and buy functions
+        require(!tradingBlocked, "Trading is currently blocked");
+        _;
+    }
+
+    function blockTrading() external onlyOwner { // call this function to lock trading
+        tradingBlocked = true;
+        emit TradingBlocked();
+    }
+
+    function unblockTrading() external onlyOwner { // call this function to unlock trading
+        tradingBlocked = false;
+        emit TradingUnblocked();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        mint(to, amount);
+    }
+
+    function burn(address addressFrom, uint256 amount) public onlyOwner{
+        burn(addressFrom, amount);
+    }
+
+    function addToken(uint256 teamID, token tokenAddress, uint256 initialPrice) public onlyOwner {
+        tokens[teamID] = TokenInfo({
+            token: tokenAddress,
+            tokenPrice: initialPrice
+        });
+    }
+
+    function buyTokens(uint256 teamID) public payable tradingNotBlocked{
+        TokenInfo storage tokenInfo = tokens[teamID];
+        uint256 amountToBuy = msg.value / tokenInfo.tokenPrice;
+        require(amountToBuy > 0, "Not enough funds sent");
+        //mint(msg.sender, amountToBuy);
+        tokenInfo.token.transfer(msg.sender, amountToBuy);
+        emit Bought(msg.sender, teamID, amountToBuy);
+        mintTokens(teamID, amountToBuy);
+    }
+
+    function sellTokens(uint256 teamID, uint256 amountToSell) public tradingNotBlocked{
+        TokenInfo storage tokenInfo = tokens[teamID];
+        require(amountToSell > 0, "You need to sell at least some tokens");
+        uint256 allowance = tokenInfo.token.allowance(msg.sender, address(this));
+        require(allowance >= amountToSell, "Check the token allowance");
+
+        tokenInfo.token.transferFrom(msg.sender, address(this), amountToSell);
+        //burn(msg.sender, amountToSell);
+        payable(msg.sender).transfer(amountToSell * tokenInfo.tokenPrice);
+        emit Sold(msg.sender, teamID, amountToSell);
+    }
+
+    function withdraw(uint256 amount) public onlyOwner {
+        require(amount <= address(this).balance, "Not enough balance");
+        payable(owner).transfer(amount);
+    }
+
+    function mintTokens(uint256 teamID, uint256 amount) public onlyOwner {
+        tokens[teamID].token.mint(address(this), amount);
+    }
+
+    function calculateNewPrice(uint256 teamID, uint256 odds, uint256 result) private view returns (uint256) {
+        uint256 newPrice;
+        uint256 currentPrice = tokens[teamID].tokenPrice;
+        if (result == 1) { // Win
+            newPrice = currentPrice + (currentPrice * odds / 100);
+        } else if (result == 2) { // Lose
+            newPrice = currentPrice - (currentPrice * odds / 100);
+        } else { // Draw
+            newPrice = currentPrice;
+        }
+        return newPrice;
+    }
+
+    function updatePrice(uint256 teamID, uint256 odds, uint256 result) public onlyOwner {
+        uint256 newPrice = calculateNewPrice(teamID, odds, result);
+        require(newPrice > 0, "New price must be greater than 0");
+        uint256 oldPrice = tokens[teamID].tokenPrice;
+        tokens[teamID].tokenPrice = newPrice;
+
+        emit PriceUpdated(teamID, oldPrice, newPrice);
     }
 }
-
-
-/* Other not integrated functions 
-
-function requestGameData(uint256 teamId, string memory date) public returns (bytes32 requestId) {
-        Chainlink.Request memory request = _buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        request._addUint("teamId", teamId);
-        request._add("date", date);
-        request._add("path", "result"); // Result key from API ? 
-        return _sendChainlinkRequestTo(oracle, request, fee);
-    }
-
-    function fulfill(bytes32 _requestId, uint256 teamId, uint256 matchResult, uint256 bettingOdds) public recordChainlinkFulfillment(_requestId) {
-        uint256 oldPrice = tokenPrices[teamId];
-        uint256 newPrice = calculateNewPrice(oldPrice, bettingOdds, matchResult);
-        tokenPrices[teamId] = newPrice;
-        emit PriceUpdated(teamId, oldPrice, newPrice);
-    }
-    
-    function calculateNewPrice(uint256 currentPrice, uint256 odds, uint256 result) private pure returns (uint256) {
-        int256 signedResult;
-        if (result == 1) { // Win
-            signedResult = 1;
-        } else if (result == 2) { // Loose
-            signedResult = -1;
-        } else { // Draw
-            signedResult = 0;
-        }
-        return uint256(int256(currentPrice) + (int256(currentPrice) * int256(odds) * signedResult / 100));
-    }
-
-*/
