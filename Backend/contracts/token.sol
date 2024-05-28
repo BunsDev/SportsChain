f5057b170a03824b933af1273920f495b5ecc37e
Gérald Quenum <geraldquenum9@gmail.com>
2024-05-28 17:34:20 +0200

    contract deploy scripts

--- Backend/contracts/token.sol
+++ Backend/contracts/token.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -8,17 +7,17 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract token is ERC20, ERC20Burnable, ERC20Pausable, AccessControl, ERC20Permit {
+contract Token is ERC20, ERC20Burnable, ERC20Pausable, AccessControl, ERC20Permit {
     address public owner;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
- constructor(string memory name, string memory symbol)
+    constructor(string memory name, string memory symbol)
         ERC20(name, symbol)
         ERC20Permit(name)
     {
         owner = msg.sender;
         _grantRole(DEFAULT_ADMIN_ROLE, owner);
-        _grantRole(MINTER_ROLE, owner); // allow the smart contract manager to mint tokens
+        _grantRole(MINTER_ROLE, owner);
         _mint(owner, 1 * 10 ** decimals());
         _mint(msg.sender, 1 * 10 ** decimals());
     }
@@ -36,7 +35,6 @@ contract token is ERC20, ERC20Burnable, ERC20Pausable, AccessControl, ERC20Permi
     }
 
     // The following functions are overrides required by Solidity.
-
     function _update(address from, address to, uint256 value)
         internal
         override(ERC20, ERC20Pausable)
